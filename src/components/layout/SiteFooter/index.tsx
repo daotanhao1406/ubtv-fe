a6205895dfcb82bd36c1c0be1f4d19c4850274e4
daotanhao1406 <daotanhao9h@gmail.com>
2024-11-12 23:07:57 +0700

    chore(global.css): change global css: background, footer background, border color

--- src/components/layout/SiteFooter/index.tsx
+++ src/components/layout/SiteFooter/index.tsx
@@ -7,7 +7,7 @@ import { siteConfig } from '@/constant/config/site'
 
 export function SiteFooter() {
   return (
-    <footer className='w-full border-t border-border/40 py-6 dark:border-border md:px-8 md:py-0'>
+    <footer className='w-full bg-footer border-border/40 py-6 dark:border-border md:px-8 md:py-0'>
       <div className='flex flex-col items-center justify-between gap-4 md:h-20 md:flex-row'>
         <p className='text-balance text-center text-sm leading-loose text-muted-foreground md:text-left'>
           Built by{' '}
