db2274a1f82a5c7dc76317607b51860281ca0e6d
daotanhao <daotanhao9h@gmail.com>
2025-05-22 23:22:37 +0700

    feat: integrate login and register api

--- src/app/(unauth)/successful-email-verification/page.tsx
+++ src/app/(unauth)/successful-email-verification/page.tsx
@@ -1,34 +1,35 @@
 'use client'
 import { Button as HeroButton } from '@heroui/react'
-import { ArrowLeft, CircleCheckBig } from 'lucide-react'
+import { CircleCheckBig } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import React from 'react'
-
-import { Button as ShadcnButton } from '@/components/ui/button'
 
 import UnAuthTemplate from '@/app/(unauth)/template'
 
 const SuccessfulEmailVerificationPage = () => {
   const router = useRouter()
-
   return (
     <UnAuthTemplate
       icon={<CircleCheckBig />}
       title='Email verified'
       description={
         <>
-          Your account has been successfully verified. <br /> Click below to login magically.
+          Your account has been successfully verified. <br /> Click below to enjoy your cinematic experience.
         </>
       }
     >
       <div className='flex flex-col items-center max-w-sm w-full mt-6'>
-        <HeroButton color='primary' type='submit' className='w-full h-12 font-bold'>
-          Continue
+        <HeroButton
+          onPress={() => {
+            router.push('/')
+            router.refresh()
+          }}
+          color='primary'
+          type='submit'
+          className='w-full h-12 font-bold'
+        >
+          Start to explore
         </HeroButton>
-        <ShadcnButton onClick={() => router.push('/login')} variant='linkHover2' className='mt-2'>
-          <ArrowLeft size={16} style={{ marginRight: 8 }} />
-          Back to Login
-        </ShadcnButton>
       </div>
     </UnAuthTemplate>
   )
